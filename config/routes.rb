8b7b40019d57896cf5842134d4510c12eaf95a0c
Matchu <matchu1993@gmail.com>
2010-10-03 20:25:02 -0400

    rearrange routes so that /item_zone_sets.js is not considered an item id

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 OpenneoImpressItems::Application.routes.draw do |map|
   match '/' => 'items#index', :as => :items
   match '/index.js' => 'items#index', :format => :js
-  match '/:id' => 'items#show', :as => :item
+ 
+  match '/item_zone_sets.js' => 'ItemZoneSets#index'
   
   match '/items/:item_id/swf_assets.json' => 'swf_assets#index', :as => :item_swf_assets
   match '/items/:item_id/bodies/:body_id/swf_assets.json' => 'swf_assets#index', :as => :item_swf_assets_for_body_id
-  match '/pet_types/:pet_type_id/swf_assets.json' => 'swf_assets#index', :as => :pet_type_swf_assets
-  
+  match '/pet_types/:pet_type_id/swf_assets.json' => 'swf_assets#index', :as => :pet_type_swf_assets  
   match '/species/:species_id/color/:color_id/pet_type.json' => 'pet_types#show'
   
-  match '/item_zone_sets.js' => 'ItemZoneSets#index'
+  match '/:id' => 'items#show', :as => :item
 end
