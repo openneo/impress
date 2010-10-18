7939b0570bba774c85226cd06f8974ec7f0eaef4
Matchu <matchu1993@gmail.com>
2010-10-18 18:10:50 -0400

    put userbar where it belongs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def auth_server_icon_url
+    # TODO: if auth servers expand, don't hardcode path
+    URI::HTTP.build(
+      :host => Openneo::Auth.config.auth_server,
+      :path => '/favicon.png'
+    ).to_s
+  end
+  
   def flashes
     flash.inject('') do |html, pair|
       key, value = pair
