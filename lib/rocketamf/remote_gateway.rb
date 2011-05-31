4e919aca04e5e36b53af4ffef37904f3863b54b8
Matchu <matchu1993@gmail.com>
2011-05-31 10:36:32 -0400

    ree compatibility

--- lib/rocketamf/remote_gateway.rb
+++ lib/rocketamf/remote_gateway.rb
@@ -1,18 +1,19 @@
 require 'net/http'
 require 'rocketamf'
-require_relative 'remote_gateway/service'
-require_relative 'remote_gateway/request'
+require File.join(File.dirname(__FILE__), 'remote_gateway', 'service')
+require File.join(File.dirname(__FILE__), 'remote_gateway', 'request')
 
 module RocketAMF
   class RemoteGateway
     attr_reader :uri
-    
+
     def initialize(url)
       @uri = URI.parse url
     end
-    
+
     def service(name)
       Service.new(self, name)
     end
   end
 end
+
