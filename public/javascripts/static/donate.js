2077ea8984775d9f70e09d645be031d049884c63
Matchu <matchu1993@gmail.com>
2011-07-01 15:18:05 -0400

    dynamic progress bar

--- public/javascripts/static/donate.js
+++ public/javascripts/static/donate.js
@@ -3,19 +3,30 @@
 
   var pledgieURL = 'http://pledgie.com/campaigns/' + PLEDGIE_CAMPAIGN_ID + '.json?callback=?';
   $.getJSON(pledgieURL, function (data) {
-    var donorsEl = $('#campaign-donors');
-    var donorsList = donorsEl.children('ol');
     var campaign = data.campaign;
-    var pledges = campaign.pledges;
 
-    var pledge, pledgeEl;
-    for(var i in pledges) {
-      pledge = pledges[i];
-      pledgeEl = $('<li/>');
-      $('<strong/>', {text: pledge.display_name}).appendTo(pledgeEl);
-      $('<span/>', {text: pledge.date}).appendTo(pledgeEl);
-      pledgeEl.appendTo(donorsList);
+    // Write the donors list if we need to
+    var donorsEl = $('#campaign-donors');
+    if(donorsEl.length) {
+      var donorsList = donorsEl.children('ol');
+      var pledges = campaign.pledges;
+
+      var pledge, pledgeEl;
+      for(var i in pledges) {
+        pledge = pledges[i];
+        pledgeEl = $('<li/>');
+        $('<strong/>', {text: pledge.display_name}).appendTo(pledgeEl);
+        $('<span/>', {text: pledge.date}).appendTo(pledgeEl);
+        pledgeEl.appendTo(donorsList);
+      }
     }
+
+    // Set campaign progress data
+    $('span.campaign-raised').text(campaign.amount_raised);
+    $('span.campaign-goal').text(campaign.goal);
+
+    var campaign_percent = campaign.amount_raised / campaign.goal * 100;
+    $('div.campaign-progress').css('width', campaign_percent + '%');
 
     $(document.body).addClass('campaign-loaded');
   });
