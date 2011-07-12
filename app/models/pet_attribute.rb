1caa29635982061f03914dc86a1f0e0dd91a284c
Matchu <matchu1993@gmail.com>
2011-07-12 18:22:16 -0400

    capitalize each word of a species/color name (for Swamp Gas)

--- app/models/pet_attribute.rb
+++ app/models/pet_attribute.rb
@@ -5,16 +5,16 @@ class PetAttribute < StaticResource
       :name => self.human_name
     }
   end
-  
+
   def human_name
-    self.name.capitalize
+    @human_name ||= self.name.split(' ').each { |word| word.capitalize! }.join(' ')
   end
-  
+
   class << self
     def all_ordered_by_name
       @objects_ordered_by_name
     end
-    
+
     def find(id)
       attribute = super
       unless attribute
@@ -24,25 +24,25 @@ class PetAttribute < StaticResource
       end
       attribute
     end
-    
+
     def find_by_name(name)
       @objects_by_name[name.downcase]
     end
   end
-  
+
   private
-  
+
   def self.data_source
     "#{to_s.downcase.pluralize}.txt"
   end
-  
+
   def self.process_line(line)
     name = line.chomp.downcase
     @objects << @objects_by_name[name] = species = new
     species.id = @objects.size
     species.name = name
   end
-  
+
   def self.fetch_objects!
     @objects = []
     @objects_by_name = {}
@@ -52,3 +52,4 @@ class PetAttribute < StaticResource
     @objects_ordered_by_name = @objects.sort { |a,b| a.name <=> b.name }
   end
 end
+
