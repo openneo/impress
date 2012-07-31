05acae3cb8943e691ac8a6ba52592d6fbd24c4bb
Matchu <matchu1993@gmail.com>
2012-07-31 10:20:37 -0400

    retroactively enqueue outfit images

--- app/models/outfit_image_update.rb
+++ app/models/outfit_image_update.rb
@@ -4,5 +4,12 @@ class OutfitImageUpdate
   def self.perform(id)
     Outfit.find(id).write_image!
   end
+  
+  # Represents an outfit image update for an outfit that existed before this
+  # feature was built. Its queue has a lower priority, so new outfits will
+  # be updated before retroactively converted outfits.
+  class Retroactive < OutfitImageUpdate
+    @queue = :retroactive_outfit_image_updates
+  end
 end
 
