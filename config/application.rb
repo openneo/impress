0db1bb8d7473c3538c502ac2575642fa35698aeb
Matchu <matchu1993@gmail.com>
2013-01-11 15:26:07 -0600

    enable fallbacks

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module OpenneoImpressItems
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.fallbacks = true
 
     # Configure generators values. Many other options are available, be sure to check the documentation.
     # config.generators do |g|
