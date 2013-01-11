acc7527b146b5d00e2b7c7c50f7222011ebd1c66
Matchu <matchu1993@gmail.com>
2013-01-11 11:48:40 -0600

    locale cookie should be long-term, not a session cookie

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -1,6 +1,6 @@
 class LocalesController < ApplicationController
   def choose
-    cookies[:locale] = params[:locale] if valid_locale?(params[:locale])
+    cookies.permanent[:locale] = params[:locale] if valid_locale?(params[:locale])
     origin = params[:return_to] || :back
     redirect_to origin
   end
