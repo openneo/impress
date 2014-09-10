2a1a3c61fa3d5a8a664245665ad525489f6d8fa9
Matchu <matchu1993@gmail.com>
2014-09-09 20:41:58 -0500

    stripe checkout image; breaks in development because dev copy of image is not public :P

--- app/assets/javascripts/static/donate.js
+++ app/assets/javascripts/static/donate.js
@@ -1,13 +1,14 @@
 (function() {
   var donationForm = document.getElementById('donation-form');
   var amountField = donationForm.amount;
-  var tokenField = donationForm.stripe_token;
+  var tokenField = donationForm.querySelector(
+    '[name=donation\\[stripe_token\\]]');
 
   var checkout = StripeCheckout.configure({
     key: 'pk_test_wEvgn4baD9W5ld5C9JCS9Ahf', // TODO
-    //image: '/square-image.png', // TODO
+    image: donationForm.getAttribute('data-checkout-image'),
     token: function(token) {
-      stripe_token.value = token.id;
+      tokenField.value = token.id;
       donationForm.submit();
     }
   });
