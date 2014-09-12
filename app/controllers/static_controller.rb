75e9fcbe5a128ab1c58d9673d111469246272188
Matchu <matchu1993@gmail.com>
2014-09-11 20:58:37 -0500

    when there are no donors yet, draw less attention to that fact :P

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,5 @@
 class StaticController < ApplicationController
   def donate
-    # TODO: scope by campaign?
     @campaign = Campaign.current
     @donations = @campaign.donations.includes(features: :outfit).
       order('created_at DESC')
