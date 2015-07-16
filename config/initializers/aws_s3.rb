0d6caceb24b7eab8ad252e05967c2f4555247f54
Matchu <matchusian@gmail.com>
2015-07-16 17:44:52 -0400

    improve app infrastructure to make a dev environment easier to reproduce
    This commit will require a few changes to the deployment process:
    * store AWS credentials in AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env vars
    * store OpenNeo Auth credentials in OPENNEO_AUTH_APP, OPENNEO_AUTH_SERVER, and OPENNEO_AUTH_SECRET env vars
    
    STRIPE_SECRET_KEY and STRIPE_PUBLISHABLE_KEY are now required, too; better have a good .env.
    You can always put in dummy values if you have no credentials, though; they only get called up if there's a transaction.
    
    SECRET_TOKEN is now an env var, too, but the production deploy process currently replaces config/initializers/secret_token.rb anyway; this might be the way to go later, though.

--- config/initializers/aws_s3.rb
+++ config/initializers/aws_s3.rb
@@ -1,8 +1,13 @@
-require 'yaml'
+access_key_id = ENV.fetch('AWS_ACCESS_KEY_ID')
+secret_access_key = ENV.fetch('AWS_SECRET_ACCESS_KEY')
 
-config = YAML.load_file Rails.root.join('config', 'aws_s3.yml')
-access_key_id = config.delete 'access_key_id'
-secret_access_key = config.delete 'secret_access_key'
+def set(params, params_key, env_key)
+  params[params_key] = ENV[env_key] if ENV.has_key?(env_key)
+end
 
-IMPRESS_S3 = RightAws::S3.new access_key_id, secret_access_key, config
+params = {}
+set params, :server, 'AWS_SERVER_HOST'
+set params, :port, 'AWS_SERVER_PORT'
+set params, :protocol, 'AWS_SERVER_PROTOCOL'
 
+IMPRESS_S3 = RightAws::S3.new access_key_id, secret_access_key, params
--- config/initializers/openneo_auth.rb
+++ config/initializers/openneo_auth.rb
@@ -1,15 +1,14 @@
 require 'openneo-auth'
-require 'yaml'
 
 Openneo::Auth.configure do |config|
-  YAML.load_file(Rails.root.join('config', 'openneo_auth.yml'))[Rails.env].each do |key, value|
-    config.send("#{key}=", value)
-  end
-  
+  config.app = ENV.fetch('OPENNEO_AUTH_APP')
+  config.auth_server = ENV.fetch('OPENNEO_AUTH_SERVER')
+  config.secret = ENV.fetch('OPENNEO_AUTH_SECRET')
+
   config.remote_auth_user_finder do |user_data|
     User.find_or_create_from_remote_auth_data(user_data)
   end
-  
+
   config.remember_user_finder do |id|
     User.find_by_id(id)
   end
