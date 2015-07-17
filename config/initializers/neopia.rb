7c2600af9f0ff7cfbfad580c8f527ff4b49ad1c7
Matchu <matchusian@gmail.com>
2015-07-16 22:37:32 -0400

    the dev version of neopia has a hostname now :D

--- config/initializers/neopia.rb
+++ config/initializers/neopia.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
   Rails.configuration.neopia_host = 'neopia.openneo.net'
 else
-  Rails.configuration.neopia_host = 'localhost:3200'
+  Rails.configuration.neopia_host = 'neopia.dev.openneo.net'
 end
