a4079d2e0c9bd1eb7755ef52723f84cf699f756d
Matchu <matchusian@gmail.com>
2015-08-30 16:49:46 -0700

    campaign 2015 copy updates

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,2 +1,9 @@
 module CampaignsHelper
+  def emote_md(text)
+    text = text.
+      gsub(/:\)/, image_tag('emoticons/smiley.gif')).
+      gsub(/:D/, image_tag('emoticons/grin.gif')).
+      gsub(/:P/, image_tag('emoticons/tongue.gif'))
+    md text
+  end
 end
