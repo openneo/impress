72a08901c81796776f8467854296b7b2a4312dbb
Matchu <matchu1993@@gmail.com>
2023-07-21 19:39:32 -0700

    Upgrade to Ruby 2.2.4, Rails 4.0.13
    NOTE: This doesn't boot yet! There's something changed in the `devise` API that we'll need to fix!
    
    ```
    /vagrant/config/initializers/devise.rb:46:in `block in <top (required)>': undefined method `encryptor=' for Devise:Module (NoMethodError)
    ```
    
    But yeah, we navigated the gem upgrades, and also I ran `rake rails:update` and hand-processed the suggestions it had for our config files.

--- config/application.rb
+++ config/application.rb
@@ -2,22 +2,15 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# If you have a Gemfile, require the gems listed there, including any gems
+# Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+Bundler.require(*Rails.groups)
 
 module OpenneoImpressItems
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # Add additional load paths for your own custom dirs
-    # config.load_paths += %W( #{config.root}/extras )
-
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running
     config.active_record.observers = [:closet_hanger_observer,
@@ -32,16 +25,6 @@ module OpenneoImpressItems
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.fallbacks = true
-
-    # Configure generators values. Many other options are available, be sure to check the documentation.
-    # config.generators do |g|
-    #   g.orm             :active_record
-    #   g.template_engine :erb
-    #   g.test_framework  :test_unit, :fixture => true
-    # end
-
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters << :password
 
     Mime::Type.register "image/gif", :gif
 
--- config/boot.rb
+++ config/boot.rb
@@ -1,14 +1,4 @@
-# Use locked gems if present.
-begin
-  require File.expand_path('../../.bundle/environment', __FILE__)
+# Set up gems listed in the Gemfile.
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
-rescue LoadError
-  # Otherwise, use RubyGems.
-  require 'rubygems'
-
-  # And set up the gems listed in the Gemfile.
-  if File.exist?(File.expand_path('../../Gemfile', __FILE__))
-    require 'bundler'
-    Bundler.setup
-  end
-end
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
