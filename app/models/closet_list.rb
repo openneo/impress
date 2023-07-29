df629368d3cf2994e43adf470518f3397fa60474
Matchu <matchu1993@@gmail.com>
2023-07-29 11:37:46 -0700

    Rename ClosetList.public -> publicly_visible
    This removes a conflict with a new Rails method `.public` on the model!

--- app/models/closet_list.rb
+++ app/models/closet_list.rb
@@ -8,7 +8,9 @@ class ClosetList < ActiveRecord::Base
   validates :hangers_owned, :inclusion => {:in => [true, false], :message => "can't be blank"}
 
   scope :alphabetical, -> { order(:name) }
-  scope :public, -> { where(arel_table[:visibility].gteq(ClosetVisibility[:public].id)) }
+  scope :publicly_visible, -> {
+    where(arel_table[:visibility].gteq(ClosetVisibility[:public].id))
+  }
   scope :visible_to, ->(user) {
     condition = arel_table[:visibility].gteq(ClosetVisibility[:public].id)
     condition = condition.or(arel_table[:user_id].eq(user.id)) if user
