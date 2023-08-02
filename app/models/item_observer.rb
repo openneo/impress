596c2a132016ee7740d5ee0d7bbccc41bc746197
Matchu <matchu1993@@gmail.com>
2023-08-02 12:02:19 -0700

    Remove unused "outfits#new newest_items" cache
    Huh! This cache key seemed to only be referenced in checks and expirations, but was never actually used! So I guess we've been loading the modeling predictions every time for a while huh??
    
    We'll get smarter about that someday, but anyway, that lets us delete our Item resque tasks and ItemObserver!

--- app/models/item_observer.rb
+++
@@ -1,9 +0,0 @@
-class ItemObserver < ActionController::Caching::Sweeper
-  def after_create(item)
-    Resque.enqueue(Item::CreateTask, item.id)
-  end
-
-  def after_destroy(item)
-    Resque.enqueue(Item::DestroyTask, item.id)
-  end
-end
