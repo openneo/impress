38f3c9894ce3c5fe8331ababda5b4903d9765ea1
Matchu <matchu1993@@gmail.com>
2023-08-02 13:05:30 -0700

    Delete WardrobeTip model
    lmao I have zero recollection of this, I've been generally trying to avoid too-far-off-the-path cleanup, but this one just seems silly, goodbye

--- app/models/wardrobe_tip.rb
+++
@@ -1,5 +0,0 @@
-class WardrobeTip < ActiveRecord::Base
-  translates :body
-
-  scope :by_index, -> { order('`index` ASC') }
-end
---
+++ db/migrate/20230802195548_drop_wardrobe_tips.rb
@@ -0,0 +1,17 @@
+class DropWardrobeTips < ActiveRecord::Migration
+  def change
+    drop_table :wardrobe_tips do |t|
+      t.integer  "index",      null: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+
+    drop_table :wardrobe_tip_translations do |t|
+      t.integer  "wardrobe_tip_id"
+      t.string   "locale"
+      t.text     "body"
+      t.datetime "created_at",      null: false
+      t.datetime "updated_at",      null: false
+    end
+  end
+end
