cf2f78703b167523bffc5f763d0dc6c2eb2a0382
Matchu <matchu1993@@gmail.com>
2023-08-02 12:04:09 -0700

    Stop caching latest contribution
    That's another tiny query that I'm okay with just removing the cache overhead complexity for right now!

--- app/models/contribution_observer.rb
+++ app/models/contribution_observer.rb
@@ -2,8 +2,6 @@ class ContributionObserver < ActiveRecord::Observer
   include FragmentExpiration
   
   def after_create(contribution)
-    expire_fragment_in_all_locales('outfits#new latest_contribution')
-    
     if contribution.contributed_type == 'SwfAsset'
       item = contribution.contributed.item
       expire_fragment_in_all_locales("items/#{item.id} contributors")
