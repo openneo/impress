caa0859a2d903539c87493912bd57b5f7271a2da
Matchu <matchu1993@@gmail.com>
2023-08-02 12:22:39 -0700

    Stop caching pet type images on item page
    That's easy queries and easy templates!

--- app/models/pet_type_observer.rb
+++
@@ -1,8 +0,0 @@
-class PetTypeObserver < ActiveRecord::Observer
-  include FragmentExpiration
-  
-  def after_create(pet_type)
-    images_key = "items/show standard_species_images special_color=#{pet_type.color_id}"
-    expire_fragment_in_all_locales(images_key)
-  end
-end
