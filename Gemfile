24e59c6258f874baa96dcb275ba5a9caa07284f0
Matchu <matchu1993@gmail.com>
2023-08-02 17:27:11 -0700

    Upgrade to Rails 6.0.6.1
    Another pretty easy one! We have the `rails app:update` changes in here too.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'http://rubygems.org'
 ruby '2.6.10'
 
-gem 'rails', '= 5.2.8.1'
+gem 'rails', '= 6.0.6.1'
 
 # Our database is MySQL, in both development and production.
 gem 'mysql2', '~> 0.5.5'
@@ -24,7 +24,7 @@ gem 'devise', '~> 4.9', '>= 4.9.2'
 gem 'will_paginate', '~> 4.0'
 
 # For translation, both for the site UI and for Neopets data.
-gem 'rails-i18n', '~> 5.1', '>= 5.1.3'
+gem 'rails-i18n', '~> 7.0', '>= 7.0.7'
 gem 'http_accept_language', '~> 2.1', '>= 2.1.1'
 gem 'globalize', '~> 6.2', '>= 6.2.1'
 
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module OpenneoImpressItems
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
