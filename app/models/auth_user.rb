eee097a9f89674c13f55ff75d725f181a3e05eb8
Matchu <matchu1993@gmail.com>
2023-08-06 16:23:22 -0700

    Sync AuthUser and User names
    Callbacks are handy for this!

--- app/models/auth_user.rb
+++ app/models/auth_user.rb
@@ -8,7 +8,23 @@ class AuthUser < AuthRecord
   validates :name, presence: true, uniqueness: {case_sensitive: false},
     length: {maximum: 20}
   
-  after_create do
+  has_one :user, foreign_key: :remote_id, inverse_of: :auth_user
+  
+  # It's important to keep AuthUser and User in sync. When we create an AuthUser
+  # (e.g. through the registration process), we create a matching User, too. And
+  # when the AuthUser's name changes, we update User to match.
+  #
+  # TODO: Should we sync deletions too? We don't do deletions anywhere in app
+  # right now, so I'll hold off to avoid leaving dead code around.
+  after_create :create_user
+  after_update :sync_name_with_user, if: :saved_change_to_name?
+
+  def create_user
     User.create(name: name, auth_server_id: 1, remote_id: id)
+  end
+
+  def sync_name_with_user
+    user.name = name
+    user.save!
   end
 end
