b2d31f619a289d69bb77d3c5f3faf863e574393f
Matchu <matchu1993@gmail.com>
2023-08-06 16:08:13 -0700

    Create a User when we create an AuthUser
    We also update seeds.rb to be up to date! This should make it possible to log in as test/test123 from a fresh database.

--- app/models/auth_user.rb
+++ app/models/auth_user.rb
@@ -7,4 +7,8 @@ class AuthUser < AuthRecord
 
   validates :name, presence: true, uniqueness: {case_sensitive: false},
     length: {maximum: 20}
+  
+  after_create do
+    User.create(name: name, auth_server_id: 1, remote_id: id)
+  end
 end
