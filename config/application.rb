e22809deb3cedace91180e451a9ac7e651b314c5
Matchu <matchu1993@gmail.com>
2023-11-11 15:12:41 -0800

    Disable some unused Rails features
    I noticed when running `rails routes` that there's a lot of routes for
    major unused Rails features, like storage. I didn't look deeply enough
    into ActiveStorage to know if I was risking accepting arbitrary file
    uploads, I just figured, if I disable it (which simplifies the app
    footprint anyway), then I can be certain! So, goodbye!

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,29 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+
+# We disable some components we don't use, to: omit their routes, be confident
+# that there's not e.g. surprise storage happening on the machine, and keep the
+# app footprint smaller.
+#
+# Disabled:
+# - active_storage/engine
+# - active_job/railtie
+# - action_cable/engine
+# - action_mailbox/engine
+# - action_text/engine
+%w(
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  rails/test_unit/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
