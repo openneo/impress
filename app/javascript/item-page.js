4c809a5f5fcd8b60ccd26e18b3497df371be4c63
Emi Matchu <emi@matchu.dev>
2024-01-19 04:29:43 -0800

    Put back the show more / show less behavior for item page trades
    I guess I deleted this a while ago without really noticing… I think I'd
    at some point like to replace this with like, the DTI 2020 improved
    table layout thing, but I figured this would be pretty quick to throw
    in and make the page not feel like a pain to use lmao

--- app/javascript/item-page.js
+++ app/javascript/item-page.js
@@ -13,3 +13,29 @@ ReactDOM.render(
   </AppProvider>,
   rootNode,
 );
+
+try {
+  const tradeHangers = document.querySelector("#trade-hangers");
+  const tradeSections = document.querySelectorAll("#trade-hangers p");
+  for (const section of tradeSections) {
+    const oneLine = parseFloat(getComputedStyle(section)['line-height']);
+    const maxHeight = oneLine * 2;
+
+    if (section.offsetHeight > maxHeight) {
+      section.style.maxHeight = `${maxHeight}px`;
+      section.setAttribute("data-overflows", "");
+    }
+
+    section.querySelector(".more")?.addEventListener("click", (event) => {
+      section.setAttribute("data-showing-more", "");
+      section.style.maxHeight = "none";
+    });
+
+    section.querySelector(".less")?.addEventListener("click", (event) => {
+      section.removeAttribute("data-showing-more");
+      section.style.maxHeight = `${maxHeight}px`;
+    });
+  }
+} catch (error) {
+  console.error("Error applying trade list more/less toggle", error);
+}
