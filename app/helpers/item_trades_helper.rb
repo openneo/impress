4b9e11fc2acf6cc30b1523b34a594db7ba0be532
Emi Matchu <emi@matchu.dev>
2024-01-21 03:59:06 -0800

    Sort trades in vaguely-recent order
    This logic is copied from DTI 2020! Though I didn't include the part
    where we highlight trade matches yet!

--- app/helpers/item_trades_helper.rb
+++ app/helpers/item_trades_helper.rb
@@ -1,2 +1,26 @@
 module ItemTradesHelper
+	def vague_trade_timestamp(last_trade_activity_at)
+		if last_trade_activity_at >= 1.week.ago
+			translate "item_trades.index.table.last_active.this_week"
+		else
+			last_trade_activity_at.strftime("%b %Y")
+		end
+	end
+
+	def sorted_vaguely_by_trade_activity(trades)
+		# First, sort the list in ascending order.
+		trades_ascending = trades.sort_by do |trade|
+			if trade.user.last_trade_activity_at >= 1.week.ago
+				# Sort recent trades in a random order, but still collectively as the
+				# most recent. (This discourages spamming updates to game the system!)
+				[1, rand]
+			else
+				# Sort older trades by last trade activity.
+				[0, trade.user.last_trade_activity_at]
+			end
+		end
+
+		# Then, reverse it!
+		trades_ascending.reverse!
+	end
 end
