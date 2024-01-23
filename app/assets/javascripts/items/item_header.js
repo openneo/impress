0011fdf76abaff916d2605c0140434daf08b6148
Emi Matchu <emi@matchu.dev>
2024-01-23 04:20:03 -0800

    Move user bulk lists form into a toggleable form, not a dialog
    My intent is to next add behavior to save this state across pageloads,
    for power users to not have to always reopen it!

--- app/assets/javascripts/items/item_header.js
+++ app/assets/javascripts/items/item_header.js
@@ -1,21 +1,12 @@
-const userListSections = document.querySelectorAll(
-	".item-header .user-lists-section");
-for (const section of userListSections) {
+const headers = document.querySelectorAll(".item-header");
+for (const header of headers) {
 	try {
-		const dialog = section.querySelector("dialog");
-		const opener = section.querySelector(".dialog-opener");
-		const closer = section.querySelector(".dialog-closer");
-		if (dialog.showModal) { // check browser support
-			opener.addEventListener("click", (event) => {
-				dialog.show();
-				event.preventDefault();
-			});
-			document.body.addEventListener("click", (event) => {
-				if (dialog.open && !section.contains(event.target)) {
-					dialog.close();
-				}
-			});
-		}
+		const form = header.querySelector(".user-lists-form");
+		const opener = header.querySelector(".user-lists-form-opener");
+		opener.addEventListener("click", (event) => {
+			form.toggleAttribute("hidden");
+			event.preventDefault();
+		});
 	} catch (error) {
 		console.error(`Error applying dialog behavior to item header:`, error);
 	}
