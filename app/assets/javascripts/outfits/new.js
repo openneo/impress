0d23412fbaa2cc83c80f3b1f1675b1cada88d23b
Emi Matchu <emi@matchu.dev>
2024-02-18 20:31:21 -0800

    Merge pet_query.js into its only call site

--- app/assets/javascripts/outfits/new.js
+++ app/assets/javascripts/outfits/new.js
@@ -1,5 +1,28 @@
 (function () {
-  // don't need to export anything in here
+  function petImage(id, size) {
+    return "https://pets.neopets.com/" + id + "/1/" + size + ".png";
+  }
+
+  var PetQuery = {},
+    query_string = document.location.hash || document.location.search;
+
+  $.each(query_string.substr(1).split("&"), function () {
+    var split_piece = this.split("=");
+    if (split_piece.length == 2) {
+      PetQuery[split_piece[0]] = split_piece[1];
+    }
+  });
+
+  if (PetQuery.name) {
+    if (PetQuery.species && PetQuery.color) {
+      $("#pet-query-notice-template")
+        .tmpl({
+          pet_name: PetQuery.name,
+          pet_image_url: petImage("cpn/" + PetQuery.name, 1),
+        })
+        .prependTo("#container");
+    }
+  }
 
   var preview_el = $("#pet-preview"),
     img_el = preview_el.find("img"),
