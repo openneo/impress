1cbcb5bcd675a58b43d09f26364a6f95c86011d9
Emi Matchu <emi@matchu.dev>
2024-04-16 16:53:30 -0700

    Add trade warning to closet list form
    Just a lil blurb to make sure it's clear that NC sales and stuff are
    forbidden! I imagine the people doing it know this, but I want to make
    sure we're being explicit, in case there's any element of
    miscommunication.

---
+++ app/assets/javascripts/closet_lists/form.js
@@ -0,0 +1,7 @@
+document.addEventListener("change", ({ target }) => {
+	if (target.matches('select[name="closet_list[visibility]"]')) {
+		target
+			.closest("form")
+			.setAttribute("data-list-visibility", target.value);
+	}
+});
