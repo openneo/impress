a55a9b08d416609b45b946a189da5a430f0f950b
Emi Matchu <emi@matchu.dev>
2024-05-18 14:29:39 -0700

    Disable sending performance traces to health.openneo.net
    There's been some stability issues with our self-hosted
    health.openneo.net service lately. I just upgraded to a new version
    today, so my hope is that today's weird slowness is that it's doing the
    expected GlitchTip 4.0 upgrade data migration process designed to save
    disk space, and hopefully it will take care of itself by the end of the
    day?
    
    But just to help out, I'm disabling this feature to remove pressure on
    the GlitchTip service. We don't actually use performance trace analysis
    irl, and so it's just taking up disk space and processing power. If we
    end up wanting to dig into something in the future, we can turn it back
    on! (My hope had been that a sample rate of 5% was small enough to be a
    good hedge to have "enough" data just in case something comes up, but
    idk, I don't actually have a great sense of how much pressure even 5%
    of our total volume is, and I'd rather just be certain it's out of the
    picture altogether, at least while working on this.)

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,4 @@
 Sentry.init do |config|
   config.dsn = 'https://2d3c5b739af149a0b3beb86a4d498e1f@health.openneo.net/1'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  config.traces_sample_rate = 0.05
 end
