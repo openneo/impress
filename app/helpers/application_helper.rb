8440a28898b02fbf5e94c79cf9e12572ed631f56
Emi Matchu <emi@matchu.dev>
2024-05-21 18:12:40 -0700

    Add cart icon to Item Getting Guide's "Buy" buttons for NC Mall

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,6 +25,7 @@ module ApplicationHelper
     klass = options.fetch(:class, "") + " button"
     link_to url, class: klass, **options do
       concat icon
+      concat " "
       concat content
     end
   end
@@ -75,6 +76,13 @@ module ApplicationHelper
     I18n.with_locale(I18n.default_locale) do
       content_for :meta, tag(:link, :rel => 'canonical', :href => url_for(resource))
     end
+  end
+
+  # SVG icon source from Font Awesome 6! (https://creativecommons.org/licenses/by/4.0/)
+  CART_ICON_SVG_SOURCE = '<path fill="currentColor" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path>'.html_safe
+  def cart_icon(alt: "Cart")
+    content_tag :svg, CART_ICON_SVG_SOURCE, alt:, class: "icon",
+      viewBox: "0 0 576 512", style: "height: 1em"
   end
 
   def contact_email
