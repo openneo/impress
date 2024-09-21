4bc38db5aa22f78d0d1c3b03a3366b17fc0fb8ee
Emi Matchu <emi@matchu.dev>
2024-09-20 19:27:39 -0700

    Replace closet_hangers/petpage.js with modern CSS
    We use jQuery to basically simulate the `:has()` pseudoselector. Let's
    just, use `:has()` now!

--- app/assets/javascripts/closet_hangers/petpage.js
+++
@@ -1,8 +0,0 @@
-(function () {
-	function setChecked() {
-		var el = $(this);
-		el.closest("li").toggleClass("checked", el.is(":checked"));
-	}
-
-	$("#petpage-closet-lists input").click(setChecked).each(setChecked);
-})();
