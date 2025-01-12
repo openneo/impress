8eb3f73deb73e8120d3354c98a86f4277f58e9cd
Emi Matchu <emi@matchu.dev>
2025-01-12 12:36:18 -0800

    Run `rails app:update` to update config files

--- config/application.rb
+++ config/application.rb
@@ -5,25 +5,18 @@ require "rails"
 # We disable some components we don't use, to: omit their routes, be confident
 # that there's not e.g. surprise storage happening on the machine, and keep the
 # app footprint smaller.
-#
-# Disabled:
-# - active_storage/engine
-# - active_job/railtie
-# - action_cable/engine
-# - action_mailbox/engine
-# - action_text/engine
-%w(
-  active_record/railtie
-  action_controller/railtie
-  action_view/railtie
-  action_mailer/railtie
-  rails/test_unit/railtie
-).each do |railtie|
-  begin
-    require railtie
-  rescue LoadError
-  end
-end
+
+# require "active_model/railtie"
+# require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -32,12 +25,12 @@ Bundler.require(*Rails.groups)
 module OpenneoImpressItems
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
